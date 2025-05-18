d50e40b2018d1b39e50fe3a2e3b3b23860eb7f88
ahmed esmai <ahmedesmailv55@gmail.com>
2025-05-18 09:04:58 +0300

    payment in controller

--- Club_System_API/Controllers/BookingsController.cs
+++ Club_System_API/Controllers/BookingsController.cs
@@ -5,6 +5,7 @@ using Club_System_API.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Stripe;
 
 namespace Club_System_API.Controllers
 {
@@ -39,6 +40,31 @@ namespace Club_System_API.Controllers
             var userId = User.GetUserId();
             var result = await _bookingService.CancelAsync(userId, bookingId);
             return result.IsSuccess ? Ok() : result.ToProblem();
+        }
+        [Authorize]
+        [HttpPost("pay/{bookingId}")]
+        public async Task<IActionResult> StartPayment(int bookingId)
+        {
+            var domain = $"{Request.Scheme}://{Request.Host}";
+            var result = await _bookingService.StartBookingPaymentAsync(User.GetUserId()!, bookingId, domain);
+            return result.IsSuccess ? Ok(new { redirectUrl = result.Value.StripeCheckoutUrl }) : result.ToProblem();
+        }
+
+        [HttpPost("webhook")]
+        public async Task<IActionResult> StripeWebhook()
+        {
+            var json = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
+            var stripeSignature = Request.Headers["Stripe-Signature"];
+
+            try
+            {
+                await _bookingService.HandleStripeWebhookAsync(json, stripeSignature!);
+                return Ok();
+            }
+            catch (StripeException ex)
+            {
+                return BadRequest(new { message = $"Stripe webhook error: {ex.Message}" });
+            }
         }
     }
 }
