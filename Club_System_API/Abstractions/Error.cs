2593a7ee053029488a6e3b18d1c988686636a86e
Adel Fayed <132665042+Adel627@users.noreply.github.com>
2025-07-16 04:07:33 +0300

    Update Error.cs

--- Club_System_API/Abstractions/Error.cs
+++ Club_System_API/Abstractions/Error.cs
@@ -3,6 +3,7 @@
     public record Error(string Code, string Description, int? StatusCode)
     {
         public static readonly Error None = new(string.Empty, string.Empty, null);
+        
     }
 
 }
